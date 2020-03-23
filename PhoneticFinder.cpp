361a20028de7f1eded79450f31f1fd87a6873a6a
hadar22 <57917375+hadar22@users.noreply.github.com>
2020-03-23 16:26:03 +0200

    Add files via upload

---
+++ PhoneticFinder.cpp
@@ -0,0 +1,12 @@
+#include "PhoneticFinder.hpp"
+
+#include <iostream>
+#include <stdexcept>
+#include <string> 
+
+using namespace std;
+//string("Dont");
+string phonetic::find(string text,string s){
+   return s;
+    }
+    
---
+++ PhoneticFinder.hpp
@@ -0,0 +1,15 @@
+// PhoneticFinder.h
+#pragma once
+
+# ifndef PHONETICFINDER_H_
+# define PHONETICFINDER_H_
+
+#include <iostream>
+#include <string>
+#include <stdexcept>
+namespace phonetic{
+    std::string find (std::string text,std::string word);
+};
+
+
+# endif
